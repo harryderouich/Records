19802322cd0317c17d9fd57665f4e4a943d125a6
harryderouich <40683@student.longroad.ac.uk>
2015-01-23 14:33:31 +0000

    1st Class Exercise, finished Rev1, has not been extended to Rev2

---
+++ Rev1.py
@@ -0,0 +1,16 @@
+#Harry Derouich
+#23/01/15
+#Records Class Exercises - Revision 1
+
+class StudentMarks:
+    def __init__(self):
+        self.StudentName = None
+        self.TestMark = None
+
+new_record = StudentMarks
+
+new_record.StudentName = input("Name: ")
+new_record.TestMark = input("Mark: ")
+
+print('{0}: {1} Marks'.format(new_record.StudentName, new_record.TestMark))
+        
